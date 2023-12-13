f45a3a285d439f7e9d8b2fdde4437d779408958c
Abiyyu Dzamir Yunas <abiyunas14@gmail.com>
2023-12-13 14:18:53 +0700

    #	modified:   .env #	modified:   Configuration/database.js

--- Configuration/database.js
+++ Configuration/database.js
@@ -22,10 +22,7 @@ module.exports = connection; */
 // Configuration/database.js
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('railway', 'root', 'AdceE1EgFbB1AcgCbF32ABFeDdfH45cB', {
-  host: 'viaduct.proxy.rlwy.net',
-  dialect: 'mysql', // sesuaikan dengan jenis database yang Anda gunakan
-});
+const db = new Sequelize(process.env.MYSQL_PRIVATE_URL);
 
 // Uji koneksi ke database
 db.authenticate()
